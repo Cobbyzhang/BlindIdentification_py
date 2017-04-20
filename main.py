7d6afe8c2ba83f5696b4aa1ec3ddc74824f187dd
CobbyMac <616759967@qq.com>
2017-04-20 23:00:29 +0800

    Nothing changed!

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ import scipy.io as scio
 
 from ErrorAnalysis.ErrorSet import ErrorSet
 
-dataFile = './Data/G1_Enumerate.mat'
+dataFile = './Data/G2_Enumerate.mat'
 data = scio.loadmat(dataFile)
 ErrorSetCell = data['ErrorSetCell'][0]
 v = data['v']
 t = data['t']
 # print ErrorSetCell
 a = ErrorSet(ErrorSetCell, v, t)
-a.show_error(2, 0)
-# print a.error_number, '\n', a.totality, '\n', a.ber
+# a.show_error(2, 0)
+print a.error_number, '\n', a.totality, '\n', t
 # print [(int(errors[:-2:2]), int(errors[1:-2:2])) for errors in ErrorSetCell[2]]
 # print [(errors[::2], errors[1::2]) for errors in ErrorSetCell[2]
 #        if int(errors[:-2:2]) == 0 or int(errors[:-2:2]) == 0]
